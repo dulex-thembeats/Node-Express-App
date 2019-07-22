3cf1d10a7660644899798d4952268b161a8a9b7e
dulex <dulex51@hotmail.com>
2019-07-22 19:15:55 +0100

    setup for heroku

--- Server.js
+++ Server.js
@@ -2,16 +2,18 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
+
 
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 //To use a middleware we do
-app.use(express.static(__dirname + '/public'));
 
-app.use((req, res) => {
-	res.render("maintenance.hbs");
-})
+// app.use((req, res, next) => {
+// 	res.render("maintenance.hbs");
+// })
+app.use(express.static(__dirname + '/public'));
 
 app.use((req, res, next) => {
 	var now = new Date().toString();
@@ -58,6 +60,6 @@ app.get('/bad', (req, res) => {
 	});
 });
 
-app.listen(3000, () => {
-	console.log('app is running on port 3000');
+app.listen(port, () => {
+	console.log(`app is running on port${port}`);
 })
